f3adfd7efa99e46c28cb131f5a16c79bb8876e2a
twistinside <twistinside@users.noreply.github.com>
2022-11-04 21:02:39 -0700

    Initial Commit

---
+++ Tests/TouchyTests/TouchyTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Touchy
+
+final class TouchyTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Touchy().text, "Hello, World!")
+    }
+}
